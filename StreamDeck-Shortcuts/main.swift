e14ca010e7cdb019c47d36e22aa4383b24f01546
SENTINELITE <SENTINELITE@users.noreply.github.com>
2022-02-07 14:48:31 -0800

    V2

--- StreamDeck-Shortcuts/main.swift
+++ StreamDeck-Shortcuts/main.swift
@@ -7,28 +7,28 @@ import Sentry
 //  | This will generate the manifest, but for now, we're manually* handling this outside... TODO: Adopt the manifest generation |
 //  ----------------------------------------------------- ----------------------------------------------------- ------------------
 
-let manifest = PluginManifest(
-    name: "StreamDeck Shortcuts",
-    description: "Launch Shortcuts straight from your StreamDeck! Features an accessibility mode for vision impaired users.",
-    author: "SENTINELITE",
-    icon: "Icons/shortcut",
-    version: "1.0.4",
-    os: [
-        .mac(minimumVersion: "12.0")
-    ],
-    software: .minimumVersion("5.0"),
-    sdkVersion: 2,
-    codePath: "StreamDeck-Shortcuts",
-    actions: [
-        PluginAction(
-            name: "Launch Shortcut",
-            uuid: "shortcut.run",
-            icon: "Icons/plus",
-            states: [
-                PluginActionState(image: "Icons/plus")
-            ],
-            tooltip: "Launches Shortcut!"),
-    ])
+//let manifest = PluginManifest(
+//    name: "StreamDeck Shortcuts",
+//    description: "Launch Shortcuts straight from your StreamDeck! Features an accessibility mode for vision impaired users.",
+//    author: "SENTINELITE",
+//    icon: "Icons/shortcut",
+//    version: "1.0.4",
+//    os: [
+//        .mac(minimumVersion: "12.0")
+//    ],
+//    software: .minimumVersion("5.0"),
+//    sdkVersion: 2,
+//    codePath: "StreamDeck-Shortcuts",
+//    actions: [
+//        PluginAction(
+//            name: "Launch Shortcut",
+//            uuid: "shortcut.run",
+//            icon: "Icons/plus",
+//            states: [
+//                PluginActionState(image: "Icons/plus")
+//            ],
+//            tooltip: "Launches Shortcut!"),
+//    ])
 
 
 //  🔷--------------------
@@ -53,5 +53,18 @@ let manifest = PluginManifest(
 //print("E: ", error)
 //sleep(20)
 
-processShortcuts()
-PluginManager.main(plugin: ShortcutsPlugin.self, manifest: manifest)
+//processShortcuts()
+//PluginManager.main(plugin: ShortcutsPlugin.self, manifest: manifest)
+
+doXy()
+
+print("")
+print("")
+print("")
+print("")
+print("...")
+print("")
+print("")
+print("")
+
+returnShortcutNameFromUUID(input: "Test Alert2")
